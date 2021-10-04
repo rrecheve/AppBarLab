08d64e4ded6c51882d6fa82f522318c9fc3f61c2
Roberto Rodríguez Echeverría <rrecheve@gmail.com>
2021-10-04 12:53:56 +0200

    updated to gradle 7.2.2, AGP 7.0.2. and androidx migration

--- app/src/main/java/es/unex/giiis/asee/appbarlab/ActivityNew.java
+++ app/src/main/java/es/unex/giiis/asee/appbarlab/ActivityNew.java
@@ -1,10 +1,8 @@
 package es.unex.giiis.asee.appbarlab;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- app/src/main/java/es/unex/giiis/asee/appbarlab/MainActivity.java
+++ app/src/main/java/es/unex/giiis/asee/appbarlab/MainActivity.java
@@ -1,14 +1,16 @@
 package es.unex.giiis.asee.appbarlab;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
